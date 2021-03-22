28c54fe04b2968195a1fc5608b7b3e71687f9d51
Arick Ahamed <arickahamed700@gmail.com>
2021-03-23 02:08:56 +0600

    signup authentication done alhamdulillah

--- script.js
+++ script.js
@@ -4,10 +4,11 @@ const loginEmail = document.getElementById('login__email');
 const loginPass = document.getElementById('login__pass');
 const gotoSignup = document.getElementById('goto-signup');
 const signupForm = document.getElementById('signup-form');
-const signupFname = document.getElementById('signup__fname');
-const signupLname = document.getElementById('signup__lname');
+const signupFirstName = document.getElementById('signup__fname');
+const signupLastName = document.getElementById('signup__lname');
 const signupEmail = document.getElementById('signup__email');
 const signupMobile = document.getElementById('signup__mobile');
+const signupPass = document.getElementById('signup__pass');
 const signupBtn = document.getElementById('signup-btn');
 const gotoLogin = document.getElementById('goto-login');
 const dashboard = document.getElementById('dashboard');
@@ -18,6 +19,12 @@ gotoSignup.addEventListener('click', function(){
     gotoSignup.style.display = 'none';
     gotoLogin.style.display = 'block';
     signupForm.style.display = 'block';
+    signupFirstName.value = '';
+    signupLastName.value = '';
+    signupEmail.value = '';
+    signupMobile.value = '';
+    signupPass.value = '';
+
 })
 
 gotoLogin.addEventListener('click', function(){
@@ -35,8 +42,6 @@ loginBtn.addEventListener('click', function(e){
             loginForm.style.display = 'none';
             dashboard.style.display = 'block';
             e.preventDefault();
-            console.log(loginEmail.value);
-            console.log(loginPass.value);
         }
     }else {
         alert('Form should not be blanked, please fill it up.');
@@ -44,9 +49,18 @@ loginBtn.addEventListener('click', function(e){
 })
 
 signupBtn.addEventListener('click', function(e){
-            signupForm.style.display = 'none';
-            dashboard.style.display = 'block';
-            e.preventDefault();
+    if (signupFirstName.value !== '' && signupFirstName.value !== 'null' && signupLastName.value !== '' && signupLastName.value !== 'null' && signupEmail.value !== '' && signupEmail.value !== 'null' && signupMobile.value !== '' && signupMobile.value !== 'null' && signupPass.value !== '' && signupPass.value !== 'null') {
+            if (signupPass.value.length < 5) {
+                alert('Your password should be grater than 5.');
+            } else {
+                signupForm.style.display = 'none';
+                dashboard.style.display = 'block';
+                e.preventDefault();
+            }
+    } else {
+        alert('You should fillup the form.');
+        e.preventDefault();
+    }
 })
 
 goBack.addEventListener('click', function(){
